cec33de3f2bdc1ae5c5db91e0b2b9b4101328aaf
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-20 22:28:45 +0100

    Add files via upload

---
+++ strings_py/strings.py
@@ -0,0 +1,17 @@
+# strings have the following qualities- 
+course = 'python for beeginners'
+# single strings 
+name = "Elijah"
+# doublee strings
+age = '''
+my name is Elijah
+
+i am eight years old
+this is m email
+ '''
+#  triple strings
+elijah= 'Tochi'
+print(elijah[1:])    
+# the block paranthesis with the colon inside it is used to copyy strings  
+
+  
